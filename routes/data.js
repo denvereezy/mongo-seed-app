77bb8c1eb437544c60fd7902d149af3d331f4eb8
denver daniels <denver@projectcodex.co>
2016-05-16 09:04:53 +0200

    update function in progress

--- routes/data.js
+++ routes/data.js
@@ -34,28 +34,64 @@ exports.show = function(req, res, next) {
             console.log(err);
         }
         var collection = db.collection('products');
-        collection.find({id}).toArray(function(err, result) {
+        collection.find({}).toArray(function(err, result) {
             // console.log(JSON.stringify(result));
             res.render('data', {
                 result: result
             });
+            db.close();
+
         });
     });
 };
 
 exports.edit = function(req,res,next) {
   var id = req.params.id;
-  console.log(id);
+  var ObjectId = require('mongodb').ObjectId;
+  // console.log(id);
   MongoClient.connect(url, function(err, db) {
       if (err) {
           console.log(err);
       }
       var collection = db.collection('products');
-      collection.find({}).toArray(function(err, result) {
+      collection.find(ObjectId(id)).toArray(function(err, result) {
           // console.log(JSON.stringify(result));
+          // console.log(err);
           res.render('edit', {
               result: result
           });
+          db.close();
+
       });
+  });
+};
+// app.get('/employee/:id/edit', function(req, res) {
+// 	employeeProvider.findById(req.param('_id'), function(error, employee) {
+// 		res.render('employee_edit',
+// 		{
+// 			title: employee.title,
+// 			employee: employee
+// 		});
+// 	});
+// });
+
+exports.update = function(req,res,next) {
+  var id = req.params.id;
+  var ObjectId = require('mongodb').ObjectId;
+  // var newId = ObjectId.createFromHexString(id);
+  // console.log(newId);
+  // console.log(newId);
+  MongoClient.connect(url, function(err, db) {
+      if (err) {
+          console.log(err);
+      }
+      var collection = db.collection('products');
+      collection.update({_id: id}, {$set: {name: req.body.name, multi:true}}, function(err, result) {
+        // console.log(err);
+          console.log(JSON.stringify(result));
+          res.redirect('/data');
+      });
+      // db.close();
+
   });
 };
