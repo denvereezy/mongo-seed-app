227979e1fb6bffaca906b426375aca900ebaa8c6
denver daniels <denver@projectcodex.co>
2016-05-16 09:29:37 +0200

    can now update and delete

--- routes/data.js
+++ routes/data.js
@@ -17,10 +17,6 @@ exports.add = function(req, res, next) {
             if (err) {
                 console.log(err);
             };
-            // collection.find({}).toArray(function(err, result) {
-            // 	console.log(JSON.stringify(result));
-            // });
-
             db.close();
 
             res.redirect('/')
@@ -35,7 +31,6 @@ exports.show = function(req, res, next) {
         }
         var collection = db.collection('products');
         collection.find({}).toArray(function(err, result) {
-            // console.log(JSON.stringify(result));
             res.render('data', {
                 result: result
             });
@@ -45,53 +40,76 @@ exports.show = function(req, res, next) {
     });
 };
 
-exports.edit = function(req,res,next) {
-  var id = req.params.id;
-  var ObjectId = require('mongodb').ObjectId;
-  // console.log(id);
-  MongoClient.connect(url, function(err, db) {
-      if (err) {
-          console.log(err);
-      }
-      var collection = db.collection('products');
-      collection.find(ObjectId(id)).toArray(function(err, result) {
-          // console.log(JSON.stringify(result));
-          // console.log(err);
-          res.render('edit', {
-              result: result
-          });
-          db.close();
+exports.edit = function(req, res, next) {
+    var id = req.params.id;
+    var ObjectId = require('mongodb').ObjectId;
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log(err);
+        }
+        var collection = db.collection('products');
+        collection.find(ObjectId(id)).toArray(function(err, result) {
+            res.render('edit', {
+                result: result
+            });
+            db.close();
 
-      });
-  });
+        });
+    });
 };
-// app.get('/employee/:id/edit', function(req, res) {
-// 	employeeProvider.findById(req.param('_id'), function(error, employee) {
-// 		res.render('employee_edit',
-// 		{
-// 			title: employee.title,
-// 			employee: employee
-// 		});
-// 	});
-// });
 
-exports.update = function(req,res,next) {
+exports.update = function(req, res, next) {
+    var id = req.params.id;
+    var ObjectId = require('mongodb').ObjectId;
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log(err);
+        }
+        var collection = db.collection('products');
+        collection.update({
+            _id: new ObjectId(id)
+        }, {
+            $set: {
+                name: req.body.name,
+                type: req.body.type,
+                gender: req.body.gender,
+                multi: true
+            }
+        }, function(err, result) {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/data');
+        });
+        db.close();
+
+    });
+};
+
+exports.delete = function(req,res,next) {
   var id = req.params.id;
   var ObjectId = require('mongodb').ObjectId;
-  // var newId = ObjectId.createFromHexString(id);
-  // console.log(newId);
-  // console.log(newId);
   MongoClient.connect(url, function(err, db) {
       if (err) {
           console.log(err);
       }
       var collection = db.collection('products');
-      collection.update({_id: id}, {$set: {name: req.body.name, multi:true}}, function(err, result) {
-        // console.log(err);
-          console.log(JSON.stringify(result));
+      collection.remove({
+          _id: new ObjectId(id)
+      }, {
+          $set: {
+              name: req.body.name,
+              type: req.body.type,
+              gender: req.body.gender,
+              multi: true
+          }
+      }, function(err, result) {
+          if (err) {
+              console.log(err);
+          }
           res.redirect('/data');
       });
-      // db.close();
+      db.close();
 
   });
 };
