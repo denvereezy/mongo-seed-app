0285d9b0d145f82011ddcfc54ce1b3fe6a78ad03
denver daniels <denver@projectcodex.co>
2016-05-17 10:56:12 +0200

    route updated

--- routes/data.js
+++ routes/data.js
@@ -1,115 +1,108 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/denver';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/denver';
+const ObjectId = require('mongodb').ObjectId;
 
 exports.add = function(req, res, next) {
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log(err, "\n");
-        }
-
-        var collection = db.collection('products');
-        collection.insert({
-            name: req.body.name,
-            type: req.body.type,
-            gender: req.body.gender
-        }, function(err, result) {
-            if (err) {
-                console.log(err);
-            };
-            db.close();
-
-            res.redirect('/')
+    MongoClient.connect(url)
+        .then(function(db) {
+            const collection = db.collection('products');
+            collection.insert({
+                    name: req.body.name,
+                    type: req.body.type,
+                    gender: req.body.gender
+                })
+                .then(function(result) {
+                    db.close();
+                    res.redirect('/');
+                })
+        })
+        .catch(function(err) {
+            next(err);
         });
-    });
 };
 
 exports.show = function(req, res, next) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log(err);
-        }
-        var collection = db.collection('products');
-        collection.find({}).toArray(function(err, result) {
-            res.render('data', {
-                result: result
-            });
-            db.close();
-
+    MongoClient.connect(url)
+        .then(function(db) {
+            const collection = db.collection('products');
+            collection.find({}).toArray()
+                .then(function(result) {
+                    res.render('data', {
+                        result: result
+                    });
+                    db.close();
+                })
+        })
+        .catch(function(err) {
+            next(err);
         });
-    });
 };
 
 exports.edit = function(req, res, next) {
     var id = req.params.id;
-    var ObjectId = require('mongodb').ObjectId;
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log(err);
-        }
-        var collection = db.collection('products');
-        collection.find(ObjectId(id)).toArray(function(err, result) {
-            res.render('edit', {
-                result: result
-            });
-            db.close();
-
+    MongoClient.connect(url)
+        .then(function(db) {
+            const collection = db.collection('products');
+            collection.find(ObjectId(id)).toArray()
+                .then(function(result) {
+                    res.render('edit', {
+                        result: result
+                    });
+                    db.close();
+                })
+        })
+        .catch(function(err) {
+            next(err);
         });
-    });
 };
 
 exports.update = function(req, res, next) {
     var id = req.params.id;
-    var ObjectId = require('mongodb').ObjectId;
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log(err);
-        }
-        var collection = db.collection('products');
-        collection.update({
-            _id: new ObjectId(id)
-        }, {
-            $set: {
-                name: req.body.name,
-                type: req.body.type,
-                gender: req.body.gender,
-                multi: true
-            }
-        }, function(err, result) {
-            if (err) {
-                console.log(err);
-            }
-            res.redirect('/data');
+    MongoClient.connect(url)
+        .then(function(db) {
+            const collection = db.collection('products');
+            collection.update({
+                    _id: new ObjectId(id)
+                }, {
+                    $set: {
+                        name: req.body.name,
+                        type: req.body.type,
+                        gender: req.body.gender,
+                        multi: true
+                    }
+                })
+                .then(function(result) {
+                    res.redirect('/data');
+                    db.close();
+                })
+        })
+        .catch(function(err) {
+            next(err);
         });
-        db.close();
-
-    });
 };
 
-exports.delete = function(req,res,next) {
-  var id = req.params.id;
-  var ObjectId = require('mongodb').ObjectId;
-  MongoClient.connect(url, function(err, db) {
-      if (err) {
-          console.log(err);
-      }
-      var collection = db.collection('products');
-      collection.remove({
-          _id: new ObjectId(id)
-      }, {
-          $set: {
-              name: req.body.name,
-              type: req.body.type,
-              gender: req.body.gender,
-              multi: true
-          }
-      }, function(err, result) {
-          if (err) {
-              console.log(err);
-          }
-          res.redirect('/data');
-      });
-      db.close();
+exports.delete = function(req, res, next) {
+    var id = req.params.id;
+    MongoClient.connect(url)
+        .then(function(db) {
+            const collection = db.collection('products');
+            collection.remove({
+                    _id: new ObjectId(id)
+                }, {
+                    $set: {
+                        name: req.body.name,
+                        type: req.body.type,
+                        gender: req.body.gender,
+                        multi: true
+                    }
+                })
+                .then(function(result) {
+                    db.close();
+                    res.redirect('/data');
+                });
 
-  });
+        })
+        .catch(function(err) {
+            next(err);
+        });
 };
